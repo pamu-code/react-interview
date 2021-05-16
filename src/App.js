2762d8907f0f32fdf9dd82c49a19c3ebdfe48a11
Pasi <pasi@localhost.localdomain>
2021-05-16 10:11:20 +0300

    Functionality part more or less completed (some things to fix)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+
+/* Global array to be moved to restricted area visible array 
 
 const todos = [
     {id: 1, name: 'Go to the supermarket', complete: false},
@@ -8,6 +10,11 @@ const todos = [
     {id: 4, name: 'Do the dishes', complete: false},
     {id: 5, name: 'Change car tyres', complete: false}
 ];
+
+*/
+
+
+/* Class-based React component left here for reference
 
 class App extends React.Component {
     constructor(props) {
@@ -56,44 +63,135 @@ class App extends React.Component {
         //implement this logic
         console.log('Remove Item!');
     }
+*/
 
-    render() {
-        return (
-            <div className="">
-                {this.todoItems()}
-                <Bar
-                    onSubmit={this.onSubmit.bind(this)}
-                    newTodoName={this.state.newTodoName}
-                    onInputChange={this.onChange.bind(this)}
-                />
-            </div>
-        );
+// Refactored functional components based implementation below
+
+
+function App (props) {
+
+//class App extends React.Component {
+//    constructor(props) {
+//        super(props);
+//        this.state = {
+//            newTodoName: '',
+//            todos: todos
+//        };
+//    }
+
+    const todos_initTable = [
+    {id: 1, name: 'Go to the supermarket', complete: false},
+    {id: 2, name: 'Call Alice', complete: false},
+    {id: 3, name: 'Ask Alice to call Bob', complete: false},
+    {id: 4, name: 'Do the dishes', complete: false},
+    {id: 5, name: 'Change car tyres', complete: false}
+    ];
+    var todoItemsList; //declaration moved here for function level visibility
+
+    const [newTodoName, set_newTodoName] = useState('')
+    const [todos, set_todos] = useState(todos_initTable)
+
+    const generateNewId = () => {
+        return (todos.length + 1);
     }
 
-    todoItems = () => {
-        var retVal = [];
+    const onSubmit = (event) => {
+        event.preventDefault();
 
-        for (let i = 0; i < this.state.todos.length; i++) {
-            var todo = this.state.todos[i];
+        let newTodos = todos.slice();
+        newTodos.push({
+            id: generateNewId(),
+            name: newTodoName,
+            complete: false
+        });
+
+        set_todos(newTodos)
+        set_newTodoName('')
+    //    this.setState({todos: newTodos, newTodoName: ''});
+    }
+
+    const onClick = (id) => {
+        todoItemsList = todos.slice();
+        for (let i = 0; i < todos.length; i++) {
+            if (todoItemsList[i].id === id) {
+                let newComplete = !todoItemsList[i].complete; //toggle
+                todoItemsList[i].complete = newComplete;
+            }
+        }
+        set_todos(todoItemsList)
+    //    this.setState({todos: todoItems});
+    }
+
+    const onChange = (event) => {
+        set_newTodoName(event.target.value)
+        //    this.setState({newTodoName: event.target.value});
+    }
+
+//TÄMÄ PITÄÄ TEHDÄ TÄHÄN!
+    const onRemoveClick = (id) => {
+        //implement this logic
+
+        todoItemsList = todos.slice();
+        for (let i = 0; i < todos.length; i++) {
+            if (todoItemsList[i].id === id) {
+                todoItemsList.splice(i, 1);
+//                let newComplete = !todoItemsList[i].complete; //toggle
+//                todoItemsList[i].complete = newComplete;
+                break;
+            }
+        }
+        set_todos(todoItemsList)
+
+
+
+
+
+
+
+
+
+
+        console.log('Remove Item!');
+    }
+
+    const todoItemsToScreen = () => {
+        let retVal = [];
+
+        for (let i = 0; i < todos.length; i++) {
+            let todo = todos[i];
             retVal.push(
                 <Hello
                     key={todo.id}
                     todo={todo}
-                    onClick={this.onClick.bind(this)}
-                    onRemoveClick={this.onRemoveClick.bind(this)}
+                    onClick={onClick}
+                    onRemoveClick={onRemoveClick}
                 />
             );
         }
         return retVal;
     };
-}
 
-class Hello extends React.Component {
-    render() {
+//    render() {
+        return (
+            <div className="">
+                {todoItemsToScreen()}
+                <Bar
+                    onSubmit={onSubmit}
+                    newTodoName={newTodoName}
+                    onInputChange={onChange}
+                />
+            </div>
+        );
+//    }
+} //React function component App
+
+//class Hello extends React.Component {
+//    render() {
+const Hello = (props) => {
         var color;
         var text;
 
-        if (this.props.todo.complete === true) {
+        if (props.todo.complete === true) {
             color = 'lightgreen';
             text = 'Complete';
         } else {
@@ -103,35 +201,37 @@ class Hello extends React.Component {
 
         return (
             <div className="wrapper" style={{backgroundColor: color}}>
-                <h3>{this.props.todo.name}</h3>
+                <h3>{props.todo.name}</h3>
                 <button
                     className="btn"
-                    onClick={() => this.props.onClick(this.props.todo.id)}>
+                    onClick={() => { 
+                        props.onClick(props.todo.id)} 
+                    }>
                     {text}
                 </button>
                 <button
                     className="btn"
-                    onClick={() =>
-                        this.props.onRemoveClick(this.props.todo.id)
+                    onClick={() => {
+                        props.onRemoveClick(props.todo.id) }
                     }>
                     Remove from list
                 </button>
             </div>
         );
-    }
-}
+    } // React function component Hello
 
-class Bar extends React.Component {
-    render() {
-        return (
+//class Bar extends React.Component {
+//    render() {
+const Bar = (props) => {
+    return (
             <form
                 className="wrapper"
                 style={{'grid-template-columns': '7fr 2fr'}}
-                onSubmit={this.props.onSubmit}>
+                onSubmit = {props.onSubmit}>
                 <input
                     placeholder="Add new todo"
-                    value={this.props.newTodoName}
-                    onChange={this.props.onInputChange}
+                    value={props.newTodoName}
+                    onChange={props.onInputChange}
                 />
                 <button
                     className="btn btn-success"
@@ -141,7 +241,6 @@ class Bar extends React.Component {
                 </button>
             </form>
         );
-    }
-}
+    } // React function component Bar
 
 export default App;
